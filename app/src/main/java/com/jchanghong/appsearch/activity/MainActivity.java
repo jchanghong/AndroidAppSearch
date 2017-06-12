da2a3a1431ecc9f41e856a6e609572fe9e8c16ab
jchanghong <3200601@qq.com>
2017-06-12 21:24:16 +0800

    init

--- app/src/main/java/com/jchanghong/appsearch/activity/MainActivity.java
+++ app/src/main/java/com/jchanghong/appsearch/activity/MainActivity.java
@@ -1,51 +1,135 @@
 package com.jchanghong.appsearch.activity;
 
 import android.annotation.SuppressLint;
-import android.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import com.jchanghong.appsearch.application.XDesktopHelperApplication;
-import com.jchanghong.appsearch.fragment.MainFragment;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import com.jchanghong.appsearch.R;
+import com.jchanghong.appsearch.adapter.AppInfoAdapter;
 import com.jchanghong.appsearch.helper.AppInfoHelper;
 import com.jchanghong.appsearch.helper.AppStartRecordHelper;
+import com.jchanghong.appsearch.model.AppInfo;
 import com.jchanghong.appsearch.service.XDesktopHelperService;
+import com.jchanghong.appsearch.util.AppUtil;
+import com.jchanghong.appsearch.util.ViewUtil;
+import com.jchanghong.appsearch.view.T9TelephoneDialpadView;
 
 @SuppressLint("ResourceAsColor")
-public class MainActivity extends BaseSingleFragmentActivity
-		 {
-
+public class MainActivity extends Activity
+		implements
+		T9TelephoneDialpadView.OnT9TelephoneDialpadView, AppInfoHelper.OnAppInfoLoad {
+	private static final String TAG="MainFragment";
+	private GridView mT9SearchGv;
+	private T9TelephoneDialpadView mT9TelephoneDialpadView;
+	private AppInfoAdapter mAppInfoAdapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 		if (AppStartRecordHelper.mInstance.mrecords == null) {
 			AppStartRecordHelper.mInstance.startLoadAppStartRecord();
 		}
 		if (!AppInfoHelper.mInstance.loaded()) {
 			XDesktopHelperService.startService(getApplicationContext());
 		}
-		super.onCreate(savedInstanceState);
+		initData();
+		mT9SearchGv = (GridView) findViewById(R.id.t9_search_grid_view);
+		mT9SearchGv.setAdapter(mAppInfoAdapter);
+		mT9TelephoneDialpadView = (T9TelephoneDialpadView)
+				findViewById(R.id.t9_telephone_dialpad_view);
+		mT9TelephoneDialpadView.mOnT9TelephoneDialpadView = this;
+		initListener();
 	}
 
-	@Override
-	protected Fragment createFragment() {
-		return  new MainFragment();
-	}
 
 	@Override
 	public void onBackPressed() {
 		runInBackgroud();
 	}
-			 @Override
-     public boolean onKeyDown(int keyCode, KeyEvent event) {
-			         if (keyCode == KeyEvent.KEYCODE_BACK) {
-				                  runInBackgroud();
-				             return true;
-				          }
-			         return super.onKeyDown(keyCode, event);
-			      }
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			runInBackgroud();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
 	private void runInBackgroud() {
 		moveTaskToBack(true);
 	}
+	public 	MainActivity() {
+
+		AppInfoHelper.mInstance.mOnAppInfoLoad = this;
+
+	}
+	@Override
+	public void onResume() {
+		super.onResume();
+		mT9TelephoneDialpadView.mT9InputEt.setText("");
+		refreshT9SearchGv();
+	}
+
+	protected void initData() {
+		mAppInfoAdapter = new AppInfoAdapter(this,
+				R.layout.app_info_grid_item, AppInfoHelper.mInstance
+				.mT9SearchAppInfos);
+	}
+
+
+	protected void initListener() {
+		mT9SearchGv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+									int position, long id) {
+				AppInfo appInfo=(AppInfo) parent.getItemAtPosition(position);
+				AppUtil.startApp(MainActivity.this, appInfo);
+			}
+		});
+	}
+
+	@Override
+	public void onDialInputTextChanged(String curCharacter) {
+		search(curCharacter);
+		refreshT9SearchGv();
+
+	}
+
+	private void search(String keyword) {
+		Log.i(TAG, "keyword=["+keyword+"]");
+		String curCharacter;
+		if (null == keyword) {
+			curCharacter = keyword;
+		} else {
+			curCharacter = keyword.trim();
+		}
+		if (TextUtils.isEmpty(curCharacter)) {
+			AppInfoHelper.mInstance.t9Search(null,false);
+		} else {
+			AppInfoHelper.mInstance.t9Search(curCharacter,false);
+		}
+	}
+
+	private void refreshT9SearchGv() {
+		BaseAdapter baseAdapter = (BaseAdapter) mT9SearchGv.getAdapter();
+ 		Log.i(TAG, "getCount"+baseAdapter.getCount()+"");
+		baseAdapter.notifyDataSetChanged();
+		ViewUtil.showView(mT9SearchGv);
+	}
+	@Override
+	public void onAppInfoLoadSuccess() {
+		refreshT9SearchGv();
+	}
+
+	@Override
+	public void onAppInfoLoadFailed() {
+
+	}
+
 }
